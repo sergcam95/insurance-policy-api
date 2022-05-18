befa4ce608a31c1262eacf3240b09d2cdc766743
Sergio Camacho <sergiocamacho0113@gmail.com>
2022-05-18 12:38:28 -0700

    3 Set up Entity Framework Core

--- InsurancePolicyService.API/Program.cs
+++ InsurancePolicyService.API/Program.cs
@@ -1,6 +1,10 @@
+using InsurancePolicyService.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddInfrastructure(configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
